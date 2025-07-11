44a5ddfe584fbbb33e7deb566c102114d8d4a13f
thezohaibkhalid <zohaibkhalid.pk@gmail.com>
2025-07-11 17:17:49 +0500

    added basic auth and now doing ui work navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="px-5 py-3">
+      <nav className="flex justify-between items-center"></nav>
+    </header>
+  );
+};
+
+export default Navbar;
